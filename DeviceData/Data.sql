58a13d9ee8794115e5c7ba31d36bf510b4ef76ad
arsenijegithub <arsenije@protonmail.com>
2023-01-23 13:17:44 +0100

    Kreirana SQL BP

---
+++ DeviceData/Data.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Data]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Type] NCHAR(11) NULL, 
+    [Code] INT NULL, 
+    [Timestamp] INT NULL, 
+    [Value] NCHAR(11) NULL, 
+    [Worktime] FLOAT NULL, 
+    [Configuration] NCHAR(11) NULL
+)
