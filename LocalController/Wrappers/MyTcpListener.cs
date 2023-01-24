bc66c7e4dbb4ec15ea6f43442bb74e8e0a5fc67d
Anabela Zonai <zonai.pr103.2020@uns.ac.rs>
2023-01-24 23:29:08 +0100

    Reimplementacija sender

--- LocalController/Wrappers/MyTcpListener.cs
+++ LocalController/Wrappers/MyTcpListener.cs
@@ -9,7 +9,38 @@ using System.Threading.Tasks;
 namespace LocalController.Wrappers
 {
     public class MyTcpListener
-    { }
+    {
+        private readonly IList<string> _contents;
+        private readonly ErrorLogger _errorLogger;
+
+        public ParallelSender(IList<string> contents)
+        {
+            _contents = contents;
+            _errorLogger = new ErrorLogger();
+        }
+
+        public void Send()
+        {
+            Console.WriteLine("Sending {0} contents...", _contents.Count);
+
+            var sendingTasks = _contents.Select(
+                    content =>
+                    {
+                        var senderTask = new SenderTask(content, _errorLogger);
+
+                        return Task.Factory.StartNew(() => senderTask.Do());
+                    }
+                );
+
+            var dumpingLogTask = Task.Factory.ContinueWhenAll(
+                                        sendingTasks.ToArray(),
+                                        completedSendingTasks => _errorLogger.Dump()
+                                    );
+
+            dumpingLogTask.Wait();
+
+            Console.WriteLine("All contents were sent with sucess");
+        }
 
       
 }
