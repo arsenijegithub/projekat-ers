59f594eb2c424407a5cc9c9328202d6f28a5fc6b
Anabela Zonai <zonai.pr103.2020@uns.ac.rs>
2023-01-24 23:29:40 +0100

    TCP client re2

--- LocalController/Wrappers/MyTcpClient.cs
+++ LocalController/Wrappers/MyTcpClient.cs
@@ -9,6 +9,29 @@ namespace LocalController.Wrappers
 {
     public class MyTcpClient
     {
-       
+        private readonly string _input;
+        private readonly ErrorLogger _errorLogger;
+
+        public SenderTask(string input, ErrorLogger errorLogger)
+        {
+            _input = input;
+            _errorLogger = errorLogger;
+        }
+
+        public bool Do()
+        {
+            try
+            {
+                Console.WriteLine("- Sending {0}", _input);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                _errorLogger.Log(String.Format("- An error sending {0}: {1}", _input, e.Message));
+
+                return false;
+            }
+        }
     }
 }
