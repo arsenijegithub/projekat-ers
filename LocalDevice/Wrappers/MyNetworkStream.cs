e5e644a63720296d5b1c62285138a8dc0e70ab06
CrazyLuks <luka.n.novakovic@gmail.com>
2023-01-28 16:27:44 +0100

    Class myNetworkStream

--- LocalDevice/Wrappers/MyNetworkStream.cs
+++ LocalDevice/Wrappers/MyNetworkStream.cs
@@ -7,8 +7,25 @@ using System.Threading.Tasks;
 
 namespace LocalDevice.Wrappers
 {
-    public class MyNetworkStream 
+    public class MyNetworkStream
     {
+	public NetworkStream Stream { get; set; }
+
+
       
+        public virtual void Write(byte[] buffer, int offset, int size)
+        {
+            Stream.Write(buffer, offset, size);
+        }
+
+        public virtual void Close()
+        {
+            Stream.Close();
+        }
+
+        public virtual int Read(byte[] buffer, int offset, int size)
+        {
+            return Stream.Read(buffer, offset, size);
+        }
     }
 }
