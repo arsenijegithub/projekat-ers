8edf67bc415881f4a93db40cb6f763dfd214436f
masicaaa <marijaostovic31@gmail.com>
2023-01-28 23:21:28 +0100

    Testiranje za hash generator

--- AssetManagementTests/LocalDeviceTest.cs
+++ AssetManagementTests/LocalDeviceTest.cs
@@ -12,7 +12,28 @@ namespace AssetManagementTests
     public class LocalDeviceTest
     {
 
+        [Test]
+        public void HashGenerator_ReturnsHash()
+        {
+            //Arrange
+            LocalDeviceClass localDevice = new LocalDeviceClass("1", "1", 1, "1", 1, "1");
+            string actualHash;
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                string Data = localDevice.Id + localDevice.Type + localDevice.Value + localDevice.WorkTime.ToString() + localDevice.Timestamp.ToString();
 
+                byte[] value = sha256.ComputeHash(Encoding.UTF8.GetBytes(Data));
+                actualHash = Encoding.UTF8.GetString(value);
+            }
+
+            //Act
+
+            string hash = localDevice.HashGenerator();
+
+            //Assert
+            Assert.AreEqual(actualHash, hash);
+
+        }
     }
 }
 
