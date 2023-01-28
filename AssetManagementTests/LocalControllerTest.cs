1ad17ecc1ea4cfa5a49faaf2e8dbf2e716a141e1
masicaaa <marijaostovic31@gmail.com>
2023-01-28 23:18:18 +0100

    Test za prijem podataka

--- AssetManagementTests/LocalControllerTest.cs
+++ AssetManagementTests/LocalControllerTest.cs
@@ -26,6 +26,41 @@ namespace AssetManagementTests
             Assert.IsNotNull(localController.MyAMSStream);
         }
 
+        [Test]
+        public void PrimiPodatke_ReturnsFalse()
+        {
+            //Arrange
+            LocalControllerClass localController = new LocalControllerClass();
+
+
+
+            var t = new Mock<MyTcpClient>();
+            t.Setup(x => x.GetStream()).Verifiable();
+
+
+            localController.MyClient = t.Object;
+            var xml = new Mock<MyXmlWriter>();
+            xml.Setup(x => x.PisiUXml(It.IsAny<LocalDeviceClass>(), It.IsAny<string>())).Verifiable();
+
+            localController.XmlWriter = xml.Object;
+
+            var myTcp = new Mock<MyTcpListener>();
+            myTcp.Setup(x => x.AcceptTcpClient()).Throws(new Exception());
+
+            localController.MyServer = myTcp.Object;
+
+
+            var stream = new Mock<MyNetworkStream>();
+            stream.Setup(x => x.Read(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<int>())).Verifiable();
+
+
+            localController.MyStream = stream.Object;
+            //Act
+            bool result = localController.PrimiPodatke();
+
+            //Assert
+            Assert.IsFalse(result);
+        }
 
 
 
